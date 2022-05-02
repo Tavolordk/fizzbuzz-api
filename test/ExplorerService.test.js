f9215127f93d76c43cf24e423367d9c728e9a1a5
Octavio Olea <oleaoctavio23@gmail.com>
2022-05-02 18:38:22 -0500

    Primera parte del proyecto fizzbuzz terminada.

---
+++ test/ExplorerService.test.js
@@ -0,0 +1,24 @@
+const ExplorerService = require("../lib/services/ExplorerService");
+
+
+
+describe("Tests para ExplorerService", () => {
+    test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    });
+
+    test("Requerimiento 2: Dame todos los explorers de una misión", () => {
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode).not.toBeUndefined();
+    });
+
+    test("Requerimiento 3: Dame todos los usuarios en una misión", () => {
+        const explorers = [{mission: "node", githubUsername:"Tavolordk"}];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorersInNode).not.toBeUndefined();
+    });
+
+});
---
+++ test/FizzbuzzService.test.js
@@ -0,0 +1,27 @@
+const FizzbuzzService = require("../lib/services/FizzbuzzService");
+
+describe("2)Probando FizzBuzz Test",()=>{
+    test("Ver si imprime Fizz, Buzz o FizzBuzz",()=>{
+        const explorer = {name:"Tavo",score:5}
+        const resultado = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(resultado.trick).toBe("BUZZ");
+    });
+
+    test("Ver si imprime Fizz, Buzz o FizzBuzz",()=>{
+        const explorer = {name:"Tavo",score:3}
+        const resultado = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(resultado.trick).toBe("FIZZ");
+    });
+
+    test("Ver si imprime Fizz, Buzz o FizzBuzz",()=>{
+        const explorer = {name:"Tavo",score:16}
+        const resultado = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(resultado.trick).toBe(16);
+    });
+
+    test("Ver si imprime Fizz, Buzz o FizzBuzz",()=>{
+        const explorer = {name:"Tavo",score:15}
+        const resultado = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(resultado.trick).toBe("FIZZBUZZ");
+    });
+})
